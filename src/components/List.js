244c8a00036e5beeaf9c0fee1a5d6c2dadf49d2d
Phurinat Puekkham <phurinaix@gmail.com>
2019-09-11 23:33:52 +0700

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Item from './Item'
 
-export default class List extends Component {
+const List = (props) => {
+  const { items, handleDelete, handleEdit } = props
+  return (
+    <ul className="list-group my-3">
+      <h3>List</h3>
+      {items.map(item=>{
+        return(
+          <Item key={item.id}
+          id={item.id}
+          name={item.name} 
+          handleDelete={handleDelete} 
+          handleEdit={handleEdit}/>
+        )
+      })}
+    </ul>
     
-  render() {
-    const {items,handleDelete,handleEdit} = this.props
-    return (
-      <ul className="list-group my-3">
-        <h3>List</h3>
-        {items.map(item=>{
-          return(
-            <Item key={item.id} 
-            name={item.name} 
-            handleDelete={handleDelete} 
-            handleEdit={handleEdit}/>
-          )
-        })}
-      </ul>
-      
-    )
-  }
+  )
 }
+
+export default List;
