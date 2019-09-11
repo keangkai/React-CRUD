860aff559f174f0fcaa0288085f8f49aeb707f71
Phurinat Puekkham <phurinaix@gmail.com>
2019-09-11 23:32:36 +0700

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Input extends Component {
-    
-    render() {
-        const {handleSubmit,item,handleChange,editItem} = this.props;
-        return (
-            <div className="card card-body my-3">
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label>Todo</label>
-                        <input type="text" className="form-control" value={item} onChange={handleChange}/>
-                    </div>
-                        <input type="submit" className="btn btn-primary" value="ADD"/>    
-                </form>
-            </div>
-        )
-    }
+const Input = (props) => {
+    const { handleSubmit, item, handleChange } = props;
+    return (
+        <div className="card card-body my-3">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Todo</label>
+                    <input type="text" className="form-control" value={item} onChange={handleChange}/>
+                </div>
+                    <input type="submit" className="btn btn-primary" value="ADD"/>    
+            </form>
+        </div>
+    )
 }
+
+export default Input;
