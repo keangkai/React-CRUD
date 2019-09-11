6823ff0a05c04eaf43c1296057ed1ba8466dd568
Phurinat Puekkham <phurinaix@gmail.com>
2019-09-11 23:33:27 +0700

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Item extends Component {
-    render() {
-        const {name,handleEdit,handleDelete}=this.props;
-        return (
-            <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-                <h6>{name}</h6>
-                <div>
-                    <span onClick={handleEdit}><i className="fas fa-edit"></i></span>
-                    <span onClick={handleDelete}><i className="fas fa-trash-alt"></i></span>
-                </div>
-            </li>
-        )
-    }
+const Item = (props) => {
+    const { id, name, handleEdit, handleDelete } = props;
+    return (
+        <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+            <h6>{name}</h6>
+            <div>
+                <span onClick={() => handleEdit(id)}><i className="fas fa-edit"></i></span>
+                <span onClick={() => handleDelete(id)}><i className="fas fa-trash-alt"></i></span>
+            </div>
+        </li>
+    )
 }
+
+export default Item;
